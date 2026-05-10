8abe093e839b8bf59854cf4974b385157b76dba9
ilyabukhantsov <ilyabukhantsvov5@gmail.com>
2026-05-11 00:50:36 +0300

    feat(db): add basic connect to DB
    Will be changed in future

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"os"
+)
 
 func main() {
-	fmt.Println("")
+
+	// Connection string: postgres://user:password@host:port/dbname
+	connString := "postgres://user:password@db:5432/mydb"
+
+	conn, err := pgx.Connect(context.Background(), connString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	fmt.Println("Successfully connected!")
 }
